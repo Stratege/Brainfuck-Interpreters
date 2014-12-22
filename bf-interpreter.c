e0fc14c5b99dfc65aa84026b9de0aa5559dbe83d
Stratege <Stratege@users.noreply.github.com>
2014-12-22 05:58:31 +0100

    added quality of life features to C version, cleaned up formating there as well

--- bf-interpreter.c
+++ bf-interpreter.c
@@ -2,66 +2,101 @@
 #include "stdbool.h"
 #include "stdio.h"
 
-void main()
+void helpSize(int* a, char* str)
 {
+	char* temp;
+	int t = strtol(str,&temp,10);
+	if(t && !(*temp))
+		(*a) = t;
+}
 
-char* array = (char*)calloc(1024,sizeof(char));
-char* end = (array+1024);
-char* commands = (char*)calloc(4096,sizeof(char));
-char* c = array;
-char* p = commands;
-int depth = 0;
-bool skip = false;
-bool forwards = true;
+void main(int argc, char **argv)
+{
+	int a = 1024;
+	int b = 4096;
+	char* filename = "input.txt";
+	if(argc > 1)
+		filename = argv[1];
+	if(argc > 2)
+		helpSize(&a,argv[2]);
+	if(argc > 3)
+		helpSize(&b,argv[3]);
 
-FILE* file = fopen("input.txt","r");
-if(file)
-{
-do
-{
-	*p = getc(file);
-	p++;
-}while(p < (commands+4096) && p != EOF);
-p = commands;
-while(*p)
-{
-switch (*p)
-{
-	case '<':
-		if(!skip && c != array) c--;
-		break;
-	case '>':
-		if(!skip && c != end) c++;
-		break;
-	case '+':
-		if(!skip) (*c)++;
-		break;
-	case '-':
-		if(!skip) (*c)--;
-		break;
-	case '.':
-		if(!skip) putchar(*c);
-		break;
-	case ',':
-		if(!skip) (*c) = getchar();
-		break;
-	case '[':
-		if(skip) {
-			if(forwards) depth++;
-			else if(!depth) {
-				skip = false; 
-				forwards = true;
-			} else depth--;
-		} else
-		if(!(*c)) skip = true;
-		break;
-	case ']':
-		if(skip) { if(forwards) {if(!depth) skip = false; else depth--;} else depth++;} else
-		if((*c)) { skip = true; forwards = false; }
-	default:
-		break;	
-}
-if(forwards) p++; else p--;
-}
-}
+
+	char* array = (char*)calloc(a,sizeof(char));
+	char* commands = (char*)calloc(b,sizeof(char));
+	char* c = array;
+	char* p = commands;
+	int depth = 0;
+	bool skip = false;
+	bool forwards = true;
+
+	FILE* file = fopen(filename,"r");
+	if(file)
+	{
+		do
+		{
+			*p = getc(file);
+			p++;
+		}while(p < (commands+b) && *p != EOF);
+		fclose(file);
+		p = commands;
+
+		while(*p)
+		{
+			switch (*p)
+			{
+				case '<':
+					if(!skip && c > array) c--;
+					break;
+				case '>':
+					if(!skip && c < (array+a)) c++;
+					break;
+				case '+':
+					if(!skip) (*c)++;
+					break;
+				case '-':
+					if(!skip) (*c)--;
+					break;
+				case '.':
+					if(!skip) putchar(*c);
+					break;
+				case ',':
+					if(!skip) (*c) = getchar();
+					break;
+				case '[':
+					if(skip) {
+						if(forwards) depth++;
+						else if(!depth) {
+							skip = false; 
+							forwards = true;
+						} else depth--;
+					} else
+					if(!(*c)) skip = true;
+					break;
+				case ']':
+					if(skip) {
+						if(forwards) {
+							if(!depth) skip = false;
+							else depth--;
+						} else depth++;
+					} else
+					if((*c)) {
+						skip = true; 
+						forwards = false; 
+					}
+				default:
+					break;	
+			}
+			if(forwards) {
+				if(p < (commands+b)) p++;
+				else break;
+			}else {
+				if(p > (commands)) p--;
+				else break;
+			}
+		}
+	}
+	free(array);
+	free(commands);
 }
